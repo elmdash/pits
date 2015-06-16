3e0b4403af960134ddd59f8b357745f501f76640
psoots <peter@castironcoding.com>
2015-06-15 20:56:57 -0700

    adding tests

---
+++ tests/Support/DateTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Peach\Tests\Support;
+
+class DateTest extends \PHPUnit_Framework_TestCase {
+
+
+    public function testItWorks() {
+        $this->assertTrue(TRUE);
+    }
+}
---
+++ tests/Support/PathTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Peach\Tests\Support;
+
+use Peach\Support\Path;
+
+class PathTest extends \PHPUnit_Framework_TestCase {
+
+
+    /** @test */
+    public function itMakesPathFromArgsVar1() {
+        $res = Path::make('some','dir','file','xml');
+        $this->assertEquals('some/dir/file.xml', $res);
+    }
+
+    /** @test */
+    public function itMakesPathFromArgsVar2() {
+        $res = Path::make('file','xml');
+        $this->assertEquals('file.xml', $res);
+    }
+
+    /** @test */
+    public function itMakesPathFromSomeArrayArgsVar1() {
+        $res = Path::make(['some','dir'],'file','xml');
+        $this->assertEquals('some/dir/file.xml', $res);
+    }
+
+    /** @test */
+    public function itMakesPathFromSomeArrayArgsVar2() {
+        $res = Path::make(['some','dir','file'],'xml');
+        $this->assertEquals('some/dir/file.xml', $res);
+    }
+
+    /** @test */
+    public function itMakesPathFromSomeArrayArgsVar3() {
+        $res = Path::make(['some','dir/file'],'xml');
+        $this->assertEquals('some/dir/file.xml', $res);
+    }
+
+    /** @test */
+    public function itMakesPathFromSomeArrayArgsVar4() {
+        $res = Path::make('some','dir','file.xml');
+        $this->assertEquals('some/dir/file.xml', $res);
+    }
+    
+}
